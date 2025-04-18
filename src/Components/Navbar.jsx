79bb6ed8a0318103f0fbd1fc25500478a4b122e5
alejoalbornoz <alejoalbornoz912@gmail.com>
2025-04-18 10:34:28 -0300

    Firts Commit

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,25 @@
+
+import style from "./Navbar.module.css"
+
+function Navbar (){
+
+
+
+
+    return (
+        <div className={style.menubar}>
+            <nav className={style.enlaces}>
+
+                <h2>MATEus Amargo</h2>
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">About Us</a></li>
+                    <li><a href="">Products</a></li>
+                    <li><a href="">Contact us</a></li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar;
