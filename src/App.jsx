67f407cd4afccb3880c6e0ed37c54e581fc63d3b
alejoalbornoz <alejoalbornoz912@gmail.com>
2025-04-21 12:04:33 -0300

    Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
 import './App.css'
 import Navbar from "/src/components/Navbar.jsx"
-
+import Home from "/src/pages/Home.jsx"
 
 function App() {
 
@@ -9,6 +9,7 @@ function App() {
   return (
     <>
       <Navbar/>
+      <Home/>
     </>
   )
 }
