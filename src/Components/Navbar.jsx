b15306723c085913b8f4ed56b2ff544e9856f75e
alejoalbornoz <alejoalbornoz912@gmail.com>
2025-04-21 10:28:45 -0300

    Nav terminado

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,5 +1,7 @@
 
 import style from "./Navbar.module.css"
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
 
 function Navbar (){
 
@@ -10,13 +12,19 @@ function Navbar (){
         <div className={style.menubar}>
             <nav className={style.enlaces}>
 
-                <h2>MATErazzi</h2>
+                
+                <h2>El Rincón del Mate</h2>
                 <ul>
                     <li><a href="">Home</a></li>
                     <li><a href="">About Us</a></li>
                     <li><a href="">Products</a></li>
                     <li><a href="">Contact us</a></li>
                 </ul>
+
+                <div className={style.btnmenu}>
+                    <a href="#" className="bi bi-person"></a>
+                    <a href=""  className="bi bi-cart3"></a>
+                </div>
             </nav>
         </div>
     )
