38df7a8bbe96d971ca808ee8bd540fbc211e7e21
alejoalbornoz <alejoalbornoz912@gmail.com>
2025-04-23 20:01:07 -0300

    Carousel

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,42 @@
 import style from "./Home.module.css";
+import { data } from "../../assets/data";
+import { useEffect, useRef, useState } from "react";
 
 function Home() {
+  // const listRef = useRef();
+  // const [currentIndex, setCurrentIndex] = useState(0);
+
+  // useEffect(() => {
+  //   const listNode = listRef.current;
+  //   const imgNode = listNode.querySelectorAll("li > img")[currentIndex];
+
+  //   if (imgNode) {
+  //     imgNode.scrollIntoView({
+  //       behavior: "smooth",
+  //     });
+  //   }
+  // }, [currentIndex]);
+
+  // const scrollToImage = (direction) => {
+  //   if (direction === "prev") {
+  //     setCurrentIndex((curr) => {
+  //       const isFirstSlide = currentIndex === 0;
+  //       return isFirstSlide ? 0 : curr - 1;
+  //     });
+  //   } else {
+  //     const isLastSlide = currentIndex === data.length - 1;
+  //     if (!isLastSlide) {
+  //       setCurrentIndex((curr) => curr + 1);
+  //     }
+  //   }
+  // };
+
   return (
     <div className={style.containerHome}>
       <div className={style.leftSide}>
         <div className={style.textHome}>
           <h1>El Rincón del Mate</h1>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, vitae tempore veniam vero harum veritatis commodi explicabo fugiat facilis fugit similique quod consectetur iusto voluptatum corporis, perferendis odit dolores. Assumenda, animi odit laborum recusandae distinctio vitae id numquam. Culpa enim aut pariatur necessitatibus dolor consequatur aliquid neque repudiandae assumenda. Itaque dolore nemo commodi, modi fugiat omnis totam natus accusantium reprehenderit aperiam facere illo consequuntur, neque, nesciunt dolorem? Explicabo quae provident a impedit! Repellendus animi consequatur ab facere nam repellat distinctio accusantium qui minima sint! Nulla, quisquam natus a dolor neque harum, repudiandae laborum perspiciatis quos officiis exercitationem nam. Debitis, voluptas.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure saepe ex nobis ab alias amet, placeat, explicabo ratione possimus blanditiis odio asperiores obcaecati minus deserunt sit, itaque voluptas? Velit deleniti est minus beatae officiis placeat expedita sit ea? Quos, ipsam, a quibusdam nobis dolor quis dignissimos, possimus nesciunt nemo beatae quia adipisci. Earum consequatur repudiandae facere itaque quia. Temporibus explicabo dolores reiciendis sed ratione rem quod. Praesentium illo suscipit nulla enim animi ut nesciunt sapiente eveniet at beatae, dolore odit mollitia libero velit nam aliquid dignissimos sit nobis dolorem! Nihil debitis ut alias delectus tempora dolorum corrupti totam consequatur nostrum.</p>
         </div>
         <div className={style.btnShopHome}>
           <button className={style.btnShopNow}>Shop now</button>
@@ -14,8 +44,20 @@ function Home() {
         </div>
       </div>
 
+      {/* <div className='leftArrow' onClick={()=> scrollToImage("prev")}>&#10092;</div>
+      <div className='rightArrow' onClick={()=> scrollToImage("next")}>&#10093;</div> */}
+
+
       <div className={style.imgMajor}>
-        <img src="/matemontaña.jpg" alt="" />
+        {/* <ul ref={listRef}>
+          {data.map((item) => {
+            return (
+              <li key={item.id} className="image-item">
+                <img src={item.imgUrl} width={500} height={750} alt="mate" />
+              </li>
+            );
+          })}
+        </ul> */}
       </div>
     </div>
   );
--- src/pages/SliderProducts/SliderProducts.jsx
+++ src/pages/SliderProducts/SliderProducts.jsx
@@ -1,10 +1,9 @@
 import style from "./SliderProducts.module.css";
 
-
-
 function SliderProducts() {
   return (
     <div className={style.containerSlider}>
+      
     </div>
   );
 }
