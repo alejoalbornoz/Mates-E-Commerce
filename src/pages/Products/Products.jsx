1dd58fd90ce9c258583eb28ca1426d124ca9d084
alejoalbornoz <alejoalbornoz912@gmail.com>
2025-04-30 12:28:24 -0300

    Update Products

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -33,6 +33,97 @@ function Products() {
             />
           </div>
         </div>
+
+        <div className={style.descriptionVector}>
+          <div className={style.descriptionGroup}>
+            <h3 className={style.descriptionMate}>Mate</h3>
+            <h3 className={style.descriptionYerba}>Yerba</h3>
+            <h3 className={style.descriptionBombilla}>Bombilla</h3>
+          </div>
+        </div>
+      </div>
+
+      {/* PRODUCTOS DESTACADOS MATE */}
+
+      <div className={style.containerProductsMates}>
+        <div className={style.productMate}>
+          <div className={style.imageMate}>
+            <div className={style.headerCenter}>
+              <h2>
+                Producto destacado <i className="bi bi-star-fill"></i>
+              </h2>
+            </div>
+
+            <div className={style.containerDestacado}>
+              <img
+                src="./src/assets/images/mateimperial.png"
+                alt=""
+                style={{ width: "300px", height: "auto" }}
+              />
+            </div>
+
+            <div className={style.textLeft}>
+              <h3>Mate imperial</h3>
+              <p>$28000</p>
+            </div>
+          </div>
+        </div>
+
+        <div className={style.productMate}>
+          <div className={style.imageMate}>
+            <div className={style.headerCenter}>
+              <h2>
+                Producto destacado <i className="bi bi-star-fill"></i>
+              </h2>
+            </div>
+
+            <div className={style.containerDestacado}>
+              <img
+                src="./src/assets/images/yerbacanarias.png"
+                alt=""
+                style={{
+                  maxWidth: "100%",
+                  width: "300px",
+                  maxHeight: "100%",
+                  objectFit: "contain",
+                }}
+              />
+            </div>
+
+            <div className={style.textLeft}>
+              <h3>Yerba Canarias 1kg</h3>
+              <p>$8000</p>
+            </div>
+          </div>
+        </div>
+
+        <div className={style.productMate}>
+          <div className={style.imageMate}>
+            <div className={style.headerCenter}>
+              <h2>
+                Producto destacado <i className="bi bi-star-fill"></i>
+              </h2>
+            </div>
+
+            <div className={style.containerDestacado}>
+              <img
+                src="./src/assets/images/bombillapicoloro.png"
+                alt=""
+                style={{
+                  maxWidth: "100%",
+                  width: "300px",
+                  maxHeight: "100%",
+                  objectFit: "contain",
+                }}
+              />
+            </div>
+
+            <div className={style.textLeft}>
+              <h3>Bombilla Pico de Loro</h3>
+              <p>$14000</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
