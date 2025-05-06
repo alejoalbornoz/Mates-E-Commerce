71a9f51e491f54a2b32d0ea79953e57a8f1c4d14
alejoalbornoz <alejoalbornoz912@gmail.com>
2025-05-06 11:49:48 -0300

    Responsive Navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,33 +1,43 @@
-
-import style from "./Navbar.module.css"
+import { useState } from "react";
+import style from "./Navbar.module.css";
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
+function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
 
-function Navbar (){
+  const toggleMenu = () => {
+    setMenuOpen(prev => !prev);
+  };
 
+  return (
+    <div className={style.menubar}>
+      <nav className={style.enlaces}>
+        <h2>El Rincón del Mate</h2>
 
-
-
-    return (
-        <div className={style.menubar}>
-            <nav className={style.enlaces}>
-
-                
-                <h2>El Rincón del Mate</h2>
-                <ul>
-                    <li><a href="">Inicio</a></li>
-                    <li><a href="">Productos</a></li>
-                    <li><a href="">Ofertas</a></li>
-                    <li><a href="">Contacto</a></li>
-                </ul>
-
-                <div className={style.btnmenu}>
-                    <a href="#" className="bi bi-person"></a>
-                    <a href=""  className="bi bi-cart3"></a>
-                </div>
-            </nav>
+        {/* Botón hamburguesa */}
+        <div className={style.menuToggle} onClick={toggleMenu}>
+          <span></span>
+          <span></span>
+          <span></span>
         </div>
-    )
+
+        {/* Enlaces */}
+        <ul className={`${style.ulBase} ${menuOpen ? style.menuActive : ""}`}>
+
+          <li><a href="">Inicio</a></li>
+          <li><a href="">Productos</a></li>
+          <li><a href="">Ofertas</a></li>
+          <li><a href="">Contacto</a></li>
+        </ul>
+
+        {/* Iconos usuario y carrito */}
+        <div className={style.btnmenu}>
+          <a href="#" className="bi bi-person"></a>
+          <a href="" className="bi bi-cart3"></a>
+        </div>
+      </nav>
+    </div>
+  );
 }
 
 export default Navbar;
