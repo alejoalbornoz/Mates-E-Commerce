a57cd7dfb3c7c736a3ddd2c095b0538ff34770f3
alejoalbornoz <alejoalbornoz912@gmail.com>
2025-06-15 19:11:34 -0300

    Update controllers

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,23 +1,43 @@
 import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
 
 export const register = async (req, res) => {
-  const { username, email, password } = req.body;
+  
+  try {
+    
+    const { username, email, password } = req.body;
+    const userFound = await User.findOne({email})
+    if (userFound) {
+      return res.status(400).json(["The email already exists"]);
+    }
 
-  const newUser = new User({
-    username,
-    email,
-    password,
-  });
+    const passwordHash = await bcrypt.hash(password, 10);
 
-  const userSaved = await newUser.save();
+    const newUser = new User({
+      username,
+      email,
+      password: passwordHash,
+    });
 
-  res.json({
-    id: userSaved._id,
-    username: userSaved.username,
-    email: userSaved.email,
-    createdAt: userSaved.createdAt,
-    updatedAt: userSaved.updatedAt,
-  });
+    const userSaved = await newUser.save();
+    const token = await createAccessToken({ id: userSaved._id });
+    res.cookie("token", token);
+
+    res.json({
+      id: userSaved._id,
+      username: userSaved.username,
+      email: userSaved.email,
+      createdAt: userSaved.createdAt,
+      updatedAt: userSaved.updatedAt,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Error registering user",
+      error: error.message,
+    });
+  }
 };
 
 export const login = (req, res) => {
