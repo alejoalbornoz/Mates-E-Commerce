dd9cec6142b2ce55221d69c6cac98d80e690e0f1
alejoalbornoz <alejoalbornoz912@gmail.com>
2025-06-17 20:14:35 -0300

    Middleware and Schema

--- src/app.js
+++ src/app.js
@@ -3,9 +3,7 @@ import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
-
 import authRoutes from "./routes/auth.routes.js";
-
 
 const app = express();
 
@@ -20,9 +18,6 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
 
-
-
 app.use("/api", authRoutes);
 
 export default app;
-
