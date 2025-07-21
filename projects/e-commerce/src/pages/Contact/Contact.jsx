3e8bd28c0ef67637cd7a3665f7f94c2cdb70c3ae
alejoalbornoz <alejoalbornoz912@gmail.com>
2025-07-21 17:24:56 -0300

    Update: Contact

--- projects/e-commerce/src/pages/Contact/Contact.jsx
+++ projects/e-commerce/src/pages/Contact/Contact.jsx
@@ -5,19 +5,17 @@ function Contact() {
     <div className="containerContact">
       <div className="containerInfo">
         <h2>Contacto</h2>
-        <div className="infoStore">
-          <h2>Podes encontrarnos en Calle falsa 123</h2>
-          <img src="src/assets/images/localMates.png" alt="" />
+        <div className="infoAndForm">
+          <div className="infoStore">
+            <h2>Podes encontrarnos en Calle falsa 123</h2>
+            <img src="src/assets/images/localMates.png" alt="Local" />
+          </div>
+          <form>
+            <input type="email" placeholder="Tu correo electrónico" required />
+            <textarea rows="4" placeholder="Escribí tu mensaje..." required></textarea>
+            <button type="submit">Enviar</button>
+          </form>
         </div>
-        <form>
-          <input type="email" placeholder="Tu correo electrónico" required />
-          <textarea
-            rows="4"
-            placeholder="Escribí tu mensaje..."
-            required
-          ></textarea>
-          <button type="submit">Enviar</button>
-        </form>
       </div>
     </div>
   );
