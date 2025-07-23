757b6040ad4d2d4f92b8ecad62b3afae2517042c
alejoalbornoz <alejoalbornoz912@gmail.com>
2025-07-23 11:24:53 -0300

    Update: PieChartBox

--- projects/e-commerce/src/Components/PieChartBox.jsx
+++ projects/e-commerce/src/Components/PieChartBox.jsx
@@ -1,6 +1,7 @@
 import { Cell, Pie, PieChart, ResponsiveContainer, Tooltip } from "recharts";
 import "./pieChartBox.css";
 
+// Datos estáticos por ahora
 const data = [
   { name: "Mates", value: 400, color: "#0088FE" },
   { name: "Yerbas", value: 300, color: "#00C49F" },
@@ -9,19 +10,22 @@ const data = [
 ];
 
 const PieChartBox = () => {
+  const total = data.reduce((acc, item) => acc + item.value, 0);
+
   return (
     <div className="pieChartBox">
-      <h1>Ventas por fuente</h1>
+      <h1>Ventas por categoría</h1>
       <div className="chart">
         <ResponsiveContainer width="99%" height={300}>
           <PieChart>
             <Tooltip
               contentStyle={{ background: "white", borderRadius: "5px" }}
+              formatter={(value, name) => [`${value}`, `${name}`]}
             />
             <Pie
               data={data}
-              innerRadius={"70%"}
-              outerRadius={"90%"}
+              innerRadius="70%"
+              outerRadius="90%"
               paddingAngle={5}
               dataKey="value"
             >
@@ -38,9 +42,8 @@ const PieChartBox = () => {
             <div className="title">
               <div className="dot" style={{ backgroundColor: item.color }} />
               <span>{item.name}</span>
-              
             </div>
-            <span>{item.value}</span>
+            <span>{((item.value / total) * 100).toFixed(1)}%</span>
           </div>
         ))}
       </div>
